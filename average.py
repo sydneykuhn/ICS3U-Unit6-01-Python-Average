a46353902de18faf6afa2083c8a8fffb406f4cea
Sydney <sydney.kuhn@mths.ca>
2021-10-21 16:10:43 +0000

    added average.py

---
+++ average.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Created by: Sydney Kuhn
+# Created on: Oct 2020
+# This program find the average of 10 random numbers
+
+import random
+
+
+def main():
+    # this function uses a list
+
+    list_of_numbers = []
+    total = 0
+
+    # input
+    for loop_counter in range(0, 10):
+        random_number = random.randint(0, 99)  # a number between 0-99
+        total = total + random_number
+        list_of_numbers.append(random_number)
+        print("The random number is : {0}".format(random_number))
+        loop_counter += 1
+
+    average = round(total / (len(list_of_numbers)), 2)
+    print("\nThe average is {0}".format(average))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
